458237c29edeba5bc298be2411853af53fd5772e
zacharykerickson <46876086+zacharykerickson@users.noreply.github.com>
2019-04-03 23:25:41 -0700

    Add files via upload

---
+++ PWP.py
@@ -0,0 +1,186 @@
+import numpy as np, warnings, seawater as sw, warnings
+
+# main, time-stepping function
+def model_timestep(T,S,U,V,z,I,L,E,P,tau_x,tau_y,dt,MLI_flux=None,Ekman_Q_flux=None,use_static_stability=True,use_mixed_layer_stability=True,use_shear_stability=True,use_Ekman_flux=False,use_MLI=False,verbose=False,I1=0.62,I2=None,lambda1=.6,lambda2=20,T0=17,S0=36,rho0=None,alpha=None,beta=None,f=sw.f(40),return_MLD=False):
+    # define initial variables
+    c = 4218 # heat capacity (J kg^-1 K^-1)
+    if I2 is None:
+        I2 = 1-I1
+    if I1+I2 != 1:
+        raise Exception('Shortwave insolation amplitudes need to sum to unity')
+    if rho0 is None:
+        rho0 = sw.dens(S0,T0,0)
+    if alpha is None:
+        alpha = -sw.alpha(S0,T0,0)*rho0 # multiply by rho to fit into nice equation of state (see get_rho function)
+    if beta is None:
+        beta = sw.beta(S0,T0,0)*rho0 # # multiply by rho to fit into nice equation of state (see get_rho function)
+    
+    dz = z[1]-z[0]
+    
+    if use_Ekman_flux and Ekman_Q_flux is None:
+        raise Exception('Using Ekman-induced buoyacy flux but no buoyancy gradients were given.')
+    if use_MLI and MLI_flux is None:
+        raise Exception('Using MLI but no horizontal buoyancy gradient given.')
+    
+    T = T.copy(); S = S.copy(); U = U.copy(); V = V.copy(); # so don't overwrite data
+    
+    # make initial heat profile
+    I_profile = -I/dz*(I1*np.exp(-z/lambda1)*(np.exp(-dz/2/lambda1)-np.exp(dz/2/lambda1))+I2*np.exp(-z/lambda2)*(np.exp(-dz/2/lambda2)-np.exp(dz/2/lambda2)))
+    L_profile = np.zeros(len(z)); L_profile[0] = L/dz
+    Q_profile = I_profile+L_profile
+    
+    if use_Ekman_flux or use_MLI:
+        mld_ind = get_mld_ind(T,S,U,V,z)
+        mld = z[mld_ind]
+    if use_Ekman_flux:
+        Q_Ek_profile = np.zeros(len(z))
+        Q_Ek_profile[0:mld_ind] = Ekman_Q_flux/mld*dz; 
+        Q_profile += Q_Ek_profile
+    if use_MLI:
+        Q_MLI_profile = np.zeros(len(z))
+        Q_MLI_profile[0:mld_ind] = MLI_flux*mld**2 /mld*dz
+        Q_profile += Q_MLI_profile
+    
+    # update temperature
+    dTdt = Q_profile/(c*rho0)
+    T += dTdt * dt
+    
+    # update salinity
+    dSdt = S[0]*(E-P)/dz/1000
+    S[0] += dSdt * dt
+    
+    # update momentum
+    # first rotate momentum halfway
+    angle = -f*dt/2 # currently assuming this is in rad
+    U,V = rotate(angle,U,V)
+    # then add wind stress
+    mld_ind = get_mld_ind(T,S,U,V,z)
+    mld = z[mld_ind]
+    U[0:mld_ind] += tau_x/mld/rho0*dz * dt; 
+    V[0:mld_ind] += tau_y/mld/rho0*dz * dt;
+    # then rotate second half
+    U,V = rotate(angle,U,V)
+    
+    if use_static_stability:
+        T,S,U,V = static_stability(T,S,U,V,z,T0,S0,rho0,alpha,beta)
+    if use_mixed_layer_stability:
+        T,S,U,V = mixed_layer_stability(T,S,U,V,z,T0,S0,rho0,alpha,beta,verbose=verbose)
+    if use_shear_stability:
+        T,S,U,V = shear_stability(T,S,U,V,z,T0,S0,rho0,alpha,beta,verbose=verbose)
+        
+    if return_MLD:
+        return T,S,U,V,get_mld(T,S,U,V,z)
+    else:
+        return T,S,U,V
+
+
+
+
+
+# function to get density (rho)    
+def get_rho(T,S,T0,S0,rho0,alpha,beta):
+    return rho0 + alpha*(T-T0) + beta*(S-S0)
+
+# function to get mld (simply calls get_mld_ind function)
+
+def get_mld(T,S,U,V,z):
+    return z[get_mld_ind(T,S,U,V,z)]
+    
+def get_mld_ind(T,S,U,V,z):
+    return np.where(np.logical_or(np.abs(T-T[0])>0.02,np.logical_or(np.abs(U-U[0])>0.01,np.abs(V-V[0])>0.01)))[0][0]
+
+def get_mld(T,S,U,V,z):
+    return z[get_mld_ind(T,S,U,V,z)]
+
+# function to get mld index as the first one where density exceeds a threshold above the density at 10 m
+def calc_mld_ind(T,S,z,T0,S0,rho0,alpha,beta):
+    rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+    z_10_ind = np.argmin(np.abs(z-10))
+    rho_10 = rho[z_10_ind]
+    ind = np.where(rho[z_10_ind::]-rho_10>0.03)[0][0]+z_10_ind
+    return ind
+
+# function to rotate zonal (U) and meridional (V) velocities by a given angle (in radians)    
+def rotate(angle,U,V):
+    temp = (U+V*1j)*np.exp(angle*1j)
+    return np.real(temp),np.imag(temp)
+
+# function to ensure static stability in the water column
+def static_stability(T,S,U,V,z,T0,S0,rho0,alpha,beta):
+    mld_ind = get_mld_ind(T,S,U,V,z)
+    rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+    if ~np.any(np.diff(rho)<0):
+        return T,S,U,V
+    T = T.copy(); S = S.copy(); U = U.copy(); V = V.copy();
+    T[0:mld_ind] = np.mean(T[0:mld_ind]); S[0:mld_ind] = np.mean(S[0:mld_ind]); 
+    U[0:mld_ind] = np.mean(U[0:mld_ind]); V[0:mld_ind] = np.mean(V[0:mld_ind])
+    rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+    while np.any(np.diff(rho)<0):
+        mld_ind += 1
+        T[0:mld_ind] = np.mean(T[0:mld_ind]); S[0:mld_ind] = np.mean(S[0:mld_ind]); 
+        U[0:mld_ind] = np.mean(U[0:mld_ind]); V[0:mld_ind] = np.mean(V[0:mld_ind])
+        rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+    return T,S,U,V
+
+# function to ensure mixed layer stability in the water column    
+def mixed_layer_stability(T,S,U,V,z,T0,S0,rho0,alpha,beta,verbose=False):
+    T = T.copy(); S = S.copy(); U = U.copy(); V = V.copy();
+    rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+    h = get_mld(T,S,U,V,z)
+    Rb = calculate_Rb(rho,h,U,V,z,rho0)
+    if verbose:
+        print('Rb: %.2f'%Rb)
+    while Rb<0.65:
+        h = get_mld(T,S,U,V,z)
+        j = np.where(z>h)[0][0]
+        U[0:j+1] = np.mean(U[0:j+1]); V[0:j+1] = np.mean(V[0:j+1]); 
+        T[0:j+1] = np.mean(T[0:j+1]); S[0:j+1] = np.mean(S[0:j+1]);
+        rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+        h = get_mld(T,S,U,V,z)
+        Rb = calculate_Rb(rho,h,U,V,z,rho0)
+        if verbose:
+            print('Rb: %.2f'%Rb)
+    return T,S,U,V
+
+# function to ensure shear stability in the water column    
+def shear_stability(T,S,U,V,z,T0,S0,rho0,alpha,beta,verbose=False,N_THRESHOLD=10000):
+    T = T.copy(); S = S.copy(); U = U.copy(); V = V.copy();
+    Rgprime = 0.3
+    rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+    h = get_mld(T,S,U,V,z)
+    Rg = calculate_Rg(rho,h,U,V,z,rho0)
+    ind = np.nanargmin(Rg)
+    if verbose:
+        print('Rg: %.2f'%Rg[ind])
+    h = get_mld(T,S,U,V,z)
+    counter = 0
+    while np.round(Rg[ind],4)<0.25:
+        U[ind],U[ind+1] = U[ind]-(1-Rg[ind]/Rgprime)*(U[ind]-U[ind+1])/2,U[ind+1]+(1-Rg[ind]/Rgprime)*(U[ind]-U[ind+1])/2
+        V[ind],V[ind+1] = V[ind]-(1-Rg[ind]/Rgprime)*(V[ind]-V[ind+1])/2,V[ind+1]+(1-Rg[ind]/Rgprime)*(V[ind]-V[ind+1])/2
+        T[ind],T[ind+1] = T[ind]-(1-Rg[ind]/Rgprime)*(T[ind]-T[ind+1])/2,T[ind+1]+(1-Rg[ind]/Rgprime)*(T[ind]-T[ind+1])/2
+        S[ind],S[ind+1] = S[ind]-(1-Rg[ind]/Rgprime)*(S[ind]-S[ind+1])/2,S[ind+1]+(1-Rg[ind]/Rgprime)*(S[ind]-S[ind+1])/2
+        rho = get_rho(T,S,T0,S0,rho0,alpha,beta)
+        Rg = calculate_Rg(rho,h,U,V,z,rho0)
+        ind = np.nanargmin(Rg)
+        if verbose:
+            print('Rg: %.2f'%Rg[ind])
+        T,S,U,V = mixed_layer_stability(T,S,U,V,z,T0,S0,rho0,alpha,beta,verbose=verbose) # remove any mixed layer instabilities
+        counter += 1
+        if counter>N_THRESHOLD:
+            warnings.warn('Could not resolve shear instability (n>%d)'%N_THRESHOLD)
+            break;
+    return T,S,U,V
+
+# calculate bulk Richardson number    
+def calculate_Rb(rho,h,U,V,z,rho0):
+    g = 9.81 # gravitational acceleration (m s^-2)
+    j = np.where(z>h)[0][0]
+    dU = U[j]-np.mean(U[0:j]); dV = V[j]-np.mean(V[0:j]); drho = rho[j]-np.mean(rho[0:j]);
+    return g*drho*h/rho0/(dU**2+dV**2) # calculate balanced Richardson number
+
+# calculate gradient Richardson number    
+def calculate_Rg(rho,h,U,V,z,rho0):
+    g = 9.81 # gravitational acceleration (m s^-2)
+    Rg = g*np.diff(rho)*np.diff(z)/rho0/(np.diff(U)**2+np.diff(V)**2)
+    Rg[z[0:-1]+np.diff(z)/2<h] = .25 # disregard these
+    return Rg
